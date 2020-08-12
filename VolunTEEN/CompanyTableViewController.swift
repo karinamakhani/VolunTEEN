ff2fd91e29c8d124532ba83c21e26cf1c73ad494
karinamakhani <69254558+karinamakhani@users.noreply.github.com>
2020-08-12 14:52:36 -0400

    Add files via upload

--- VolunTEEN/CompanyTableViewController.swift
+++ VolunTEEN/CompanyTableViewController.swift
@@ -27,7 +27,7 @@ class CompanyTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return listOfOpps.count
     }
 
 
@@ -47,19 +47,10 @@ class CompanyTableViewController: UITableViewController {
         return cell
     }
 
-
-    
-
-   
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let nextAddOpp = segue.destination as? AddOppViewController {
+            nextAddOpp.previousOppTVC = self
+        }
     }
-    */
 
 }
